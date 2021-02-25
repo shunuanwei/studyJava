1ff84ff50c00e52c331b42ad6ad658f71dbde34e
weish <weishunuan@homedone.net>
2021-02-25 09:18:14 +0800

    知识增加

---
+++ study/src/main/java/demo/scannner/get.java
@@ -0,0 +1,15 @@
+package demo.scannner;
+
+import java.util.Scanner;
+
+public class get {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String next = scanner.next();
+        String next1 = scanner.nextLine();
+
+        System.out.println("==>"+next);
+    }
+}
